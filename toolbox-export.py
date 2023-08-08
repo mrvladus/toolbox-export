17da09679643a2e21c151818d24b9f1d2ec3d468
mrvladus <mrvladus@yandex.ru>
2023-08-08 17:23:59 +0300

    Update

--- toolbox-export.py
+++ toolbox-export.py
@@ -92,10 +92,10 @@ def export() -> None:
             files.extend(
                 glob.glob(f"/usr/share/pixmaps/**/{icon_name}.*", recursive=True)
             )
-            icon_dir: str = os.path.expanduser("~") + "/.local/share/icons"
+            icon_dir: str = f"{os.path.expanduser('~')}/.local/share/icons"
             for file in files:
                 if file.endswith("png") or file.endswith("svg"):
-                    file_name = icon_dir + "/" + file.split("/")[-1]
+                    file_name = f"{icon_dir}/{file.split('/')[-1]}"
                     shutil.copy(file, file_name)
                     info(f"Exported icon: {file_name}")
 
